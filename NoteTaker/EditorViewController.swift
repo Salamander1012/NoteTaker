e385da6a60fe03cfe7d395eff40d48c19fe25b53
Salman Fakhri <salmanfakhri2@gmail.com>
2018-03-24 18:30:10 -0400

    attemp editor with textView

--- NoteTaker/EditorViewController.swift
+++ NoteTaker/EditorViewController.swift
@@ -11,37 +11,34 @@ import WebKit
 
 class EditorViewController: UIViewController {
 
-    @IBOutlet weak var webview: WKWebView!
+    
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var toolBar: UIView!
     
     var toolBarBottomConstraint: NSLayoutConstraint?
-    var webviewConstraints: NSLayoutConstraint?
+    var textViewConstraints: NSLayoutConstraint?
+    
+    var attributedText = NSMutableAttributedString(string: "Hello World", attributes: [
+        NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 16)
+    ])
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        UITextView.appearance().tintColor = .black
         
         toolBarBottomConstraint = NSLayoutConstraint(item: toolBar, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
         
-        webviewConstraints = NSLayoutConstraint(item: webview, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        textViewConstraints = NSLayoutConstraint(item: textView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
         
-        if let path = Bundle.main.path(forResource: "index", ofType: "html") {
-            let url = URL(fileURLWithPath: path)
-            let urlRequest = URLRequest(url: url)
-            webview.load(urlRequest)
-            
-        }
         
+        textView.attributedText = attributedText
         
         view.addConstraint(toolBarBottomConstraint!)
         
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-        
-//        webview.contentMode = .scaleToFill
-//        webview.scrollView.bounces = false
-//        webview.scrollView.layer.masksToBounds = false
-//
+  
         toolBar.addTopBorderWithColor(color: UIColor(red:0.91, green:0.91, blue:0.91, alpha:1.0), width: 1)
         
 
@@ -56,9 +53,8 @@ class EditorViewController: UIViewController {
             let isKeyboardShowing = notification.name == NSNotification.Name.UIKeyboardWillShow
             
             toolBarBottomConstraint?.constant = isKeyboardShowing ? -keyboardFrame!.height : 0
-            webviewConstraints?.constant = isKeyboardShowing ? -keyboardFrame!.height - 30 : 0
-            webview.scrollView.setNeedsFocusUpdate()
-            webview.contentMode = .scaleToFill
+            textViewConstraints?.constant = isKeyboardShowing ? -keyboardFrame!.height - 30 : 0
+            
             UIView.animate(withDuration: 0, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
                 self.view.layoutIfNeeded()
             }, completion: {(completed) in })
@@ -67,23 +63,33 @@ class EditorViewController: UIViewController {
     }
     
     @IBAction func listPressed(_ sender: Any) {
-        webview.evaluateJavaScript("document.execCommand(\"insertUnorderedList\", false, null)") { (asd, error) in
-            print("insert list")
-        }
+//        webview.evaluateJavaScript("document.execCommand(\"insertUnorderedList\", false, null)") { (asd, error) in
+//            print("insert list")
+//        }
     }
     
     @IBAction func h1Pressed(_ sender: Any) {
-        webview.evaluateJavaScript("document.execCommand(\"formatBlock\", false, \"H1\")") { (asd, error) in
-            print("insert H1")
-        }
+//        webview.evaluateJavaScript("document.execCommand(\"formatBlock\", false, \"H1\")") { (asd, error) in
+//            print("insert H1")
+//        }
+        attributedText.append(NSMutableAttributedString(string: "", attributes: [
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 24)
+        ]))
         
+        textView.attributedText = attributedText
     }
     
     @IBAction func h2Pressed(_ sender: Any) {
-        webview.evaluateJavaScript("document.execCommand(\"formatBlock\", false, \"H2\")") { (asd, error) in
-            print("insert H2")
-        }
+//        webview.evaluateJavaScript("document.execCommand(\"formatBlock\", false, \"H2\")") { (asd, error) in
+//            print("insert H2")
+//        }
     }
+    
+    
+    @IBAction func donePressed(_ sender: Any) {
+        textView.endEditing(true)
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -101,6 +107,12 @@ class EditorViewController: UIViewController {
     }
     */
 
+}
+
+extension EditorViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView.text)
+    }
 }
 
 extension UIView {
